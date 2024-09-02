179b7095dd8a3246905ce36af85ef575eb811471
Erin Van Brunt <77031790+AlrightErin27@users.noreply.github.com>
2024-09-01 19:25:12 -0700

    still buggy crossword

--- src/Components/Games/Crossword/Crossword.jsx
+++ src/Components/Games/Crossword/Crossword.jsx
@@ -5,9 +5,11 @@ import Modal from "./BuffyModal";
 import BuffyLogo from "../../../Images/Games/Crossword/logo.png";
 
 export default function Crossword() {
+  // const inputRef = useRef("");
   const [squares, setSquares] = useState([]);
   const [clicks, setClicks] = useState(0);
   const [selModal, setSelModal] = useState(false);
+  const [nextGridIdx, setNextGridIdx] = useState(null);
 
   const answers = [
     {
@@ -96,98 +98,14 @@ export default function Crossword() {
     },
   ];
 
+  //TO DO: make handleUserInput change nextGridIdx only when fresh char has been entered
+  //or if the square's char is already filled-> skip over
+
   //fills squares instate with data trickled down from questions arr
   useEffect(() => {
-    const gameData = [
-      {
-        dir: "hor",
-        local: 257,
-        num: 12,
-        q: "What is the name of the high school that Buffy attends in the first three seasons?",
-        a: "Sunnydale High",
-      },
-      {
-        dir: "vert",
-        local: 44,
-        num: 2,
-        q: "What is the full name of Buffy’s best friend who eventually becomes a powerful witch?",
-        a: "Willow Rosenberg",
-      },
-      {
-        dir: "hor",
-        local: 99,
-        num: 5,
-        q: "What is the first name of Buffy’s sister who is introduced in Season 5?",
-        a: "Dawn",
-      },
-      {
-        dir: "vert",
-        local: 52,
-        num: 3,
-        q: "What is the name of the Hellmouth that Sunnydale is built over?",
-        a: "Boca del Infierno",
-      },
-      {
-        dir: "hor",
-        local: 232,
-        num: 10,
-        q: "What is the last name of the principal who replaces Principal Flutie at Sunnydale High and becomes one of Buffy’s adversaries?",
-        a: "Snyder",
-      },
-      {
-        dir: "hor",
-        local: 187,
-        num: 9,
-        q: "In Season 5, Buffy's sister is revealed to be The ___, which has the power to open dimensional portals.",
-        a: "Key",
-      },
-      {
-        dir: "vert",
-        local: 185,
-        num: 8,
-        q: "The ___ is the name of the local nightclub in Sunnydale where Buffy and her friends often hang out?",
-        a: "Bronze",
-      },
-      {
-        dir: "vert",
-        local: 120,
-        num: 6,
-        q: "Which friendly demon, known for his loose, floppy skin and love of kittens, becomes friends with Buffy and the Scooby Gang?",
-        a: "Clem",
-      },
-      {
-        dir: "hor",
-        local: 73,
-        num: 4,
-        q: "___ ___ Spike nickname was given to Spike during his human days before he was turned into a vampire?",
-        a: "Rail Road",
-      },
-      {
-        dir: "vert",
-        local: 9,
-        num: 1,
-        q: "What is Gile's first name?",
-        a: "Rupert",
-      },
-      {
-        dir: "vert",
-        local: 247,
-        num: 11,
-        q: "Who was the vampire that sired Angel, turning him into a vampire in the 18th century?",
-        a: "Darla",
-      },
-      {
-        dir: "hor",
-        local: 129,
-        num: 7,
-        q: "Processed ___ matter was the shocking secret ingredient in the burgers served at the Doublemeat Palace",
-        a: "Vegetable",
-      },
-    ];
-
     //create a new words array that contains:
     //the word, its direction, its question # & its location on the grid
-    const words = gameData.map((data) => {
+    const words = answers.map((data) => {
       //change answer to lowercase and any remove white spaces
       let altAnswer = data.a.replace(/ /g, "").toLocaleLowerCase();
       return {
@@ -197,7 +115,6 @@ export default function Crossword() {
         dir: data.dir,
       };
     });
-    // console.log(...words);
 
     //create new letters array that contains:
     //char, word, dir, q, grid_ID & corner piece boolean
@@ -254,7 +171,6 @@ export default function Crossword() {
         if (matchingLetter.corner) {
           square.corner = true;
         }
-
         // Set black to false
         square.black = false;
       });
@@ -265,8 +181,36 @@ export default function Crossword() {
     setSquares(alteredSquares);
   }, []);
 
+  function handleUserInput(e, sq) {
+    console.log(sq);
+    setNextGridIdx(null);
+    console.log(e);
+
+    //set currSq and find what will be next squares dom input
+    for (let i = 0 + sq.grid_ID; i < squares.length; i++) {
+      //if the next id has HL, and square in sq array includes the selSquare's word
+      //and if their grid_IDs do not match
+      if (
+        squares[i].highLight &&
+        squares[i].words.includes(sq.words[0] || sq.words[1]) &&
+        squares[i].grid_ID !== sq.grid_ID
+      ) {
+        setNextGridIdx(squares[i].grid_ID);
+        break;
+      }
+    }
+  }
+  if (nextGridIdx) {
+    console.log("NEXT:", nextGridIdx);
+    let nextInput = document.querySelector(
+      `input.input[data-grid-id="${nextGridIdx}"]`
+    );
+    nextInput.focus();
+  }
+
   function clickSquare(selSq) {
-    console.log("SELECT", selSq.grid_ID, ...selSq.words);
+    // console.log("SELECT", selSq);
+    setNextGridIdx(null);
 
     let arrHL = squares.map((sq) => {
       let direction = clicks === 0 ? "hor" : "vert";
@@ -283,7 +227,6 @@ export default function Crossword() {
           return { ...sq, highLight: true };
         }
       }
-
       return { ...sq, highLight: false };
     });
 
@@ -338,7 +281,7 @@ export default function Crossword() {
                   sq={sq}
                   key={sq.grid_ID}
                   clickSquare={clickSquare}
-                  squares={squares}
+                  handleUserInput={handleUserInput}
                 />
               );
             })}
--- src/Components/Games/Crossword/Square.jsx
+++ src/Components/Games/Crossword/Square.jsx
@@ -1,13 +1,11 @@
 import "./Square.css";
 import { useState, useEffect, useRef } from "react";
 
-export default function Square({ sq, clickSquare, squares }) {
+export default function Square({ sq, clickSquare, handleUserInput }) {
   const inputRef = useRef("");
   const [text, setText] = useState("");
   const [sqCorrect, setSqCorrect] = useState(false);
   const [doubleClick, setDoubleClick] = useState(false);
-  const [nextID, setNextID] = useState(null);
-  const [nextDOM, setNextDOM] = useState(null);
 
   useEffect(() => {
     if (text.toLocaleLowerCase() === sq.chars[0]) {
@@ -18,42 +16,12 @@ export default function Square({ sq, clickSquare, squares }) {
       setText(sq.chars[0]);
       setSqCorrect(true);
     }
-
-    findNextID();
   }, [text, doubleClick, sq.chars]);
-
-  if (nextDOM !== null) {
-    console.log("NEXT_ID_DOM: ", nextDOM.value);
-    nextDOM.focus();
-  }
-
-  function findNextID() {
-    if (text !== "") {
-      for (let i = 0 + sq.grid_ID; i < squares.length; i++) {
-        if (
-          sq.words.includes(squares[i].words[0] || squares[i].words[1]) &&
-          sq.grid_ID !== squares[i].grid_ID
-        ) {
-          setNextDOM(
-            document.querySelector(
-              `input.input[data-grid-id="${squares[i].grid_ID}"]`
-            )
-          );
-          console.log(
-            document.querySelector(
-              `input.input[data-grid-id="${squares[i].grid_ID}"]`
-            )
-          );
-          break;
-        }
-      }
-    }
-    return null;
-  }
 
   function handleChange(e) {
     setText(e.target.value);
-    console.log(e);
+    // console.log(e);
+    handleUserInput(e, sq);
   }
 
   function handleDoubleClick() {
@@ -82,18 +50,17 @@ export default function Square({ sq, clickSquare, squares }) {
               ? { backgroundColor: "rgba(211, 222, 61, 0.59)" }
               : null
           }
-          onClick={() => {
-            clickSquare(sq);
-          }}
         >
           {displayCorner()}
           <input
             onDoubleClickCapture={() => handleDoubleClick()}
+            onClick={() => {
+              clickSquare(sq);
+            }}
             className="input"
             style={sqCorrect ? { color: "red", pointerEvents: "none" } : null}
             value={text.toLocaleUpperCase()}
             maxLength={1}
-            // onChange={(e) => setText(e.target.value)}
             onChange={(e) => handleChange(e)}
             ref={inputRef}
             data-grid-id={sq.grid_ID}
