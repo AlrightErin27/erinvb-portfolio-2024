99f8acf3b0110c76752ae6be6cf06cc1589c5ed2
Erin Van Brunt <77031790+AlrightErin27@users.noreply.github.com>
2024-09-22 17:39:40 -0700

    changed styling on noughts and crosses. dark and floral

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -24,41 +24,43 @@ const Blog = () => {
   }, []);
 
   return (
-    <div className="blog-container">
-      <div className="blog-content">
-        <div className="title">
-          <a
-            href="https://medium.com/@erinmontybruce"
-            target="_blank"
-            rel="noreferrer"
-            className="title-a"
-          >
-            <div>
-              Erin's Medium Blog
-              <img
-                src={`${authorImg}`}
-                alt="author img"
-                className="author-img"
-              />
-            </div>
-          </a>
-        </div>
+    <div className="blog-overlay">
+      <div className="blog-container">
+        <div className="blog-content">
+          <div className="title">
+            <a
+              href="https://medium.com/@erinmontybruce"
+              target="_blank"
+              rel="noreferrer"
+              className="title-a"
+            >
+              <div>
+                Erin's Medium Blog
+                <img
+                  src={`${authorImg}`}
+                  alt="author img"
+                  className="author-img"
+                />
+              </div>
+            </a>
+          </div>
 
-        <div className="blog-block">
-          {posts.map((post) => (
-            <div className="posts" key={post.guid}>
-              <h2>{post.title}</h2>
-              <p dangerouslySetInnerHTML={{ __html: post.content }} />
-              <a
-                href={post.link}
-                target="_blank"
-                rel="noreferrer"
-                className="posts-a"
-              >
-                Read more
-              </a>
-            </div>
-          ))}
+          <div className="blog-block">
+            {posts.map((post) => (
+              <div className="posts" key={post.guid}>
+                <h2>{post.title}</h2>
+                <p dangerouslySetInnerHTML={{ __html: post.content }} />
+                <a
+                  href={post.link}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="posts-a"
+                >
+                  Read more
+                </a>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
