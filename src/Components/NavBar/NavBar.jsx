fba01bef372be9ad1c4ada6185624a96898b904c
Erin Van Brunt <77031790+AlrightErin27@users.noreply.github.com>
2024-11-04 12:03:39 -0800

    changes to NavBar.css to fix responsive sizing using clam and a row break in Navbar.jsx. Fix Nest Hub layout for home component

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,42 +3,48 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 const NavBar = () => {
-  const [select, setSelect] = useState("home");
+  const [select, setSelect] = useState("home"); // State to track the selected navigation link
 
   const components = [
     { title: "Home", path: "/" },
     { title: "Resume", path: "/resume" },
-    { title: "Project Videos", path: "/project-videos" },
+    {
+      title: [
+        <span key="1">Project</span>,
+        <br key="2" />,
+        <span key="3">Walkthrough Videos</span>,
+      ],
+      path: "/project-videos",
+    },
     { title: "Blog", path: "/blog" },
     { title: "Games", path: "/games" },
     { title: "Shop", path: "/shop" },
   ];
 
-  if (select)
-    return (
-      <nav className="vintage-nav">
-        <ul className="nav-ul">
-          {components.map((comp, idx) => {
-            return (
-              <li
-                key={idx}
-                className={`nav-li ${select === comp.title ? "select-li" : ""}`}
-              >
-                <Link
-                  to={comp.path}
-                  onClick={() => setSelect(comp.title)}
-                  className={`nav-link ${
-                    select === comp.title ? "select-link" : ""
-                  }`}
-                >
-                  {comp.title}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-    );
+  return (
+    <nav className="vintage-nav">
+      <ul className="nav-ul">
+        {components.map((comp, idx) => (
+          // Each navigation item is a list item
+          <li
+            key={idx}
+            className={`nav-li ${select === comp.title ? "select-li" : ""}`} // Apply 'select-li' class if the item is selected
+          >
+            {/* Link component for navigation */}
+            <Link
+              to={comp.path} // Path for the navigation link
+              onClick={() => setSelect(comp.title)}
+              className={`nav-link ${
+                select === comp.title ? "select-link" : ""
+              }`}
+            >
+              {comp.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
