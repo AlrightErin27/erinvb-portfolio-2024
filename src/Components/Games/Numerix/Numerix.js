052d23f69397d25cbbd38ae51ee46afe4f1c51b5
Erin Van Brunt <77031790+AlrightErin27@users.noreply.github.com>
2025-01-07 19:26:04 -0800

    TESTING deploy db connection still bad, removed /api from Numerix.js

--- src/Components/Games/Numerix/Numerix.js
+++ src/Components/Games/Numerix/Numerix.js
@@ -289,16 +289,13 @@ export default function Numerix() {
   // Handle saving the score
   async function handleSaveScore() {
     try {
-      console.log(
-        "Attempting to save score to:",
-        `${API_URL}/api/numerix/score`
-      );
+      console.log("Attempting to save score to:", `${API_URL}/numerix/score`);
       console.log("With data:", {
         numerixUsername: username,
         numerixScore: score,
       });
 
-      const response = await axios.post(`${API_URL}/api/numerix/score`, {
+      const response = await axios.post(`${API_URL}/numerix/score`, {
         numerixUsername: username,
         numerixScore: score,
       });
